4df6b1d52e69bfbb390184c155e92580b2ba40ae
Fredrik Danielsen <fredrik.danielsen2@amedia.no>
2018-02-20 14:59:06 +0100

    Add adplogger analytics adapter

---
+++ modules/adploggerAnalyticsAdapter.js
@@ -0,0 +1,92 @@
+/**
+ * ga.js - analytics adapter for google analytics
+ */
+
+var events = require('src/events');
+var utils = require('src/utils');
+var CONSTANTS = require('src/constants.json');
+var adaptermanager = require('src/adaptermanager');
+
+var BID_REQUESTED = CONSTANTS.EVENTS.BID_REQUESTED;
+var BID_TIMEOUT = CONSTANTS.EVENTS.BID_TIMEOUT;
+var BID_RESPONSE = CONSTANTS.EVENTS.BID_RESPONSE;
+
+exports.enableAnalytics = function ({ provider, options }) {
+
+    var existingEvents = events.getEvents();
+
+    var bid = null;
+
+    utils._each(existingEvents, function (eventObj) {
+      if (typeof eventObj !== 'object') {
+        return;
+      }
+      var args = eventObj.args;
+
+      if (eventObj.eventType === BID_REQUESTED) {
+        bid = args;
+        sendBidRequestToGa(bid);
+      } else if (eventObj.eventType === BID_RESPONSE) {
+        // bid is 2nd args
+        bid = args;
+        sendBidResponseToGa(bid);
+      } else if (eventObj.eventType === BID_TIMEOUT) {
+        const bidderArray = args;
+        sendBidTimeouts(bidderArray);
+      }
+    });
+
+    events.on(BID_REQUESTED, function (bidRequestObj) {
+      sendBidRequestToGa(bidRequestObj);
+    });
+
+    events.on(BID_RESPONSE, function (bid) {
+      sendBidResponseToGa(bid);
+    });
+
+    events.on(BID_TIMEOUT, function (bidderArray) {
+      sendBidTimeouts(bidderArray);
+    });
+
+  // finally set this function to return log message, prevents multiple adapter listeners
+  this.enableAnalytics = function _enable() {
+    return utils.logMessage(`Analytics adapter already enabled, unnecessary call to \`enableAnalytics\`.`);
+  };
+};
+
+function sendBidRequestToGa(bid) {
+  if (bid && bid.bidderCode) {
+    logToAdp("a_prebid_bid_request", bid.bidderCode);
+  }
+}
+
+function sendBidResponseToGa(bid) {
+  if (bid && bid.bidderCode) {
+
+      if (bid.cpm > 0) {
+        logToAdp("a_prebid_bid", [bid.cpm, bid.bidderCode, bid.adUnitCode]);
+        logToAdp("a_prebid_bid_load_time", [bid.timeToRespond, bid.bidderCode, bid.adUnitCode]);
+      }
+  }
+}
+
+function logToAdp(event, data) {
+  utils.logMessage("Sending prebid event to adplogger [" + event + "][" + data + "]");
+  window.postMessage({
+    adpEventName: 'seshat-add',
+    data: {
+      [event]: data,
+    },
+  }, '*');
+}
+
+function sendBidTimeouts(timedOutBidders) {
+  utils._each(timedOutBidders, function (bidderCode) {
+    logToAdp("a_prebid_timeout", bidderCode);
+  });
+}
+
+adaptermanager.registerAnalyticsAdapter({
+  adapter: exports,
+  code: 'adplogger'
+});
---
+++ modules/adploggerAnalyticsAdapter.md
@@ -0,0 +1,9 @@
+# Overview
+
+Module Name: Adplogger Analytics Adapter
+Module Type: Analytics Adapter
+Maintainer: Amedia
+
+# Description
+
+Analytics adapter for Adplogger.
